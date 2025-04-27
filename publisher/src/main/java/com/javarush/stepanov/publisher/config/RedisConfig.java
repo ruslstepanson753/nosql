591ddf909f775b6cb2bbf43b9b3b97c3309f089f
Ruslan Stepanov <ruslstepanson753@gmail.com>
2025-04-27 16:21:11 +0300

    redis connect

---
+++ publisher/src/main/java/com/javarush/stepanov/publisher/config/RedisConfig.java
@@ -0,0 +1,66 @@
+package com.javarush.stepanov.publisher.config;
+
+import com.javarush.stepanov.publisher.model.notice.Kafka;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.connect-timeout}")
+    private Long timeout;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        var configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        var lettuceClientConfiguration = LettuceClientConfiguration
+                .builder()
+                .commandTimeout(Duration.ofMillis(timeout))
+                .build();
+        return new LettuceConnectionFactory(configuration, lettuceClientConfiguration);
+    }
+
+    @Bean
+    public <K, V> RedisTemplate<K, V> redisTemplate() {
+        RedisTemplate<K, V> redisTemplate = new RedisTemplate<>();
+        LettuceConnectionFactory lettuceConnectionFactory = lettuceConnectionFactory();
+        lettuceConnectionFactory.afterPropertiesSet();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        var serializer = new GenericJackson2JsonRedisSerializer();
+        redisTemplate.setKeySerializer(serializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    public static void main(String[] args) {
+        RedisConfig redisConfig = new RedisConfig();
+        redisConfig.host = "127.0.0.1";
+        redisConfig.port = 6379;
+        redisConfig.timeout = 600L;
+        RedisTemplate <String, Kafka> redisTemplate = redisConfig.redisTemplate();
+        Kafka kafka = new Kafka();
+        kafka.setId(1L);
+        redisTemplate.opsForValue().set("key", kafka);
+        String value = redisTemplate.opsForValue().get("key").toString();
+        System.out.println(value);
+
+    }
+}
